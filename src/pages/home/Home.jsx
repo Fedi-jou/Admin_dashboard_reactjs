c8e643fd5588ac567eaf3f0e1da49979b9d71117
Fedi <fedijou@gmail.com>
2022-04-17 17:06:25 +0100

    home page init

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar';
 import "./home.scss";
 
 export const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <Sidebar/>
+      <div className="homecontainer">container</div>
+    </div>
   )
 }
