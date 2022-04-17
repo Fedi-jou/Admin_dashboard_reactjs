ee6a7f6b87192c7753e15d775ac878278366feea
Fedi <fedijou@gmail.com>
2022-04-17 18:08:28 +0100

    navbar import

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
 import "./home.scss";
 
 export const Home = () => {
   return (
     <div className='home'>
       <Sidebar/>
-      <div className="homecontainer">container</div>
+      <div className="homecontainer"><Navbar/>
+      Home container ..
+      </div>
     </div>
   )
 }
