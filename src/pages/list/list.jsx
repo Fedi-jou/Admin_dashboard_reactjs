e7d2479ffb4192c3fc0f52bf8a60f6aca05d8b01
Fedi <fedijou@gmail.com>
2022-04-18 19:18:17 +0100

    datatable add to list page

--- src/pages/list/list.jsx
+++ src/pages/list/list.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import "./list.scss"
+import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
+import DataTable from '../../components/datatable/Datatable';
 
 export const List = () => {
   return (
-    <div>list</div>
+    <div className="list"><Sidebar/>
+    <div className="listContainer">
+    <Navbar/>
+    <DataTable/>
+    </div>
+    </div>
   )
 }
