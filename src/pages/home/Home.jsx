406264a4765667c313b0fb8777b1197519e841d9
Fedi <fedijou@gmail.com>
2022-04-18 02:44:48 +0100

    widget import and props add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import "./home.scss";
+import Widget from '../../components/widget/Widget';
 
 export const Home = () => {
   return (
     <div className='home'>
       <Sidebar/>
       <div className="homecontainer"><Navbar/>
-      Home container ..
+      <div className="widgets">
+        <Widget type="user"/>
+        <Widget type="order"/>
+        <Widget type ="earning"/>
+        <Widget type ="balance"/>
+      </div>
+      <div className="charts">
+        
+      </div>
       </div>
     </div>
   )
