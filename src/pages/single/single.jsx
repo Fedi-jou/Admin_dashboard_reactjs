0c2905ca7dff9efb48817fad19ece35546954c90
Fedi <fedijou@gmail.com>
2022-04-19 19:05:25 +0100

    single item creation

--- src/pages/single/single.jsx
+++ src/pages/single/single.jsx
@@ -1,7 +1,52 @@
 import React from 'react'
-
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import Chart from "../../components/chart/Chart"
+import List from "../../components/table/Table"
+import "./single.scss"
 export const Single = () => {
   return (
-    <div>single</div>
+    <div className="single">
+      <Sidebar/>
+      <div className="singleContainer">
+        <Navbar/>
+       <div className="top">
+         <div className="left">
+           <div className="editButton">Edit</div>
+           <h1 className="title">Information</h1>
+           <div className="item">
+             <img src="https://www.kindpng.com/picc/m/163-1636340_user-avatar-icon-avatar-transparent-user-icon-png.png" alt="single item" className="itemImg" />
+           <div className="details">
+             <h1 className="itemTitle">Jane Doe</h1>
+             <div className="detailItem">
+               <span className="itemKey">Email :</span>
+               <span className="itemValue">janedoe@gmail.com</span>
+             </div>
+             <div className="detailItem">
+               <span className="itemKey">Phone :</span>
+               <span className="itemValue">00 000 000 00</span>
+             </div>
+             <div className="detailItem">
+               <span className="itemKey">Address :</span>
+               <span className="itemValue">Reactjs street </span>
+             </div>
+             <div className="detailItem">
+               <span className="itemKey">Country :</span>
+               <span className="itemValue">Doe's city</span>
+             </div>
+           </div>
+           </div>
+
+         </div>
+         <div className="right">
+           <Chart aspect={3/1} title="Users Spending Last 6 Months"/>
+         </div>
+       </div>
+       <div className="bottom">
+       <h1 className="title">Last Transactions</h1>
+         <List/>
+       </div>
+      </div>
+      </div>
   )
 }
