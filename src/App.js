c43215392531387ad016ca41789230986366e475
Fedi <fedijou@gmail.com>
2022-04-20 17:36:21 +0100

    props add to products n users forms

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { List } from "./pages/list/List";
 import { Single } from "./pages/single/Single";
 import { New } from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { userInputs, productInputs } from "./formSource";
 
 function App() {
   return (
@@ -17,12 +18,18 @@ function App() {
           <Route path="users">
             <Route index element={<List />} />
             <Route path=":userId" element={<Single />} />
-            <Route path="new" element={<New />} />
+            <Route
+              path="new"
+              element={<New inputs={userInputs} title="user" />}
+            />
           </Route>
           <Route path="products">
             <Route index element={<List />} />
             <Route path=":productId" element={<Single />} />
-            <Route path="new" element={<New />} />
+            <Route
+              path="new"
+              element={<New inputs={productInputs} title="product" />}
+            />
           </Route>
         </Routes>
       </BrowserRouter>
