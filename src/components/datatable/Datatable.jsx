2e552a39c186bb4fb480a3ca87cae79ca6811096
Fedi <fedijou@gmail.com>
2022-04-20 18:16:59 +0100

    link to newuser form

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./datatable.scss"
 import { DataGrid } from '@mui/x-data-grid';
 import { userColumns , userRows } from '../../datatablesource';
+import {Link} from "react-router-dom";
 
 
 const DataTable = () => {
@@ -13,8 +14,12 @@ const DataTable = () => {
       renderCell : ()=> {
         return (
           <div className='cellAction'>
+            <Link to="/users/test" style={{textDecoration : "none"}}>
             <div className="viewButton">View</div>
+            </Link>
+            <Link to="/users/test" style={{textDecoration : "none"}}>
             <div className="deleteButton">Delete</div>
+            </Link>
           </div>
         )
       }
@@ -24,6 +29,12 @@ const DataTable = () => {
  
   return (
     <div className='datatable'>
+      <div className="datatableTitle">
+        Add New User
+        <Link to="/users/new" className="link">
+          Add New
+        </Link>
+      </div>
 <DataGrid
         rows={userRows}
         columns={userColumns.concat(actionColumn)}
