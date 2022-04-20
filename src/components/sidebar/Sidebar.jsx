c5785a0f60d1ff610497c45913f91f7e22e56b40
Fedi <fedijou@gmail.com>
2022-04-20 17:49:06 +0100

    sidebar Link add

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -12,25 +12,38 @@ import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
 import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import {Link} from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
-      <div className="top"><span className="logo"> company logo </span></div>
-      <hr/>
-      <div className="center">
-        <ul>
-          <p className="title">MAIN</p>
-         <li>
-            <DashboardIcon className='icon'/>
-           <span>Dashboard</span></li>
-           <p className="title">LIST</p>
-         <li>
-           <PersonOutlineIcon className='icon'/>
-           <span>Users</span></li>
-         <li><StoreIcon className='icon'/>
-           <span>Products</span></li>
-         <li>
+    <div className="sidebar">
+    <div className="top">
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <span className="logo">lamadmin</span>
+      </Link>
+    </div>
+    <hr />
+    <div className="center">
+      <ul>
+        <p className="title">MAIN</p>
+        <li>
+          <DashboardIcon className="icon" />
+          <span>Dashboard</span>
+        </li>
+        <p className="title">LISTS</p>
+        <Link to="/users" style={{ textDecoration: "none" }}>
+          <li>
+            <PersonOutlineIcon className="icon" />
+            <span>Users</span>
+          </li>
+        </Link>
+        <Link to="/products" style={{ textDecoration: "none" }}>
+          <li>
+            <StoreIcon className="icon" />
+            <span>Products</span>
+          </li>
+        </Link>
+        <li>
            <CreditCardIcon className='icon'/>
            <span>Orders</span></li>
          <li><LocalShippingIcon className='icon'/>
